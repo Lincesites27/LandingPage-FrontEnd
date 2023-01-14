3d9c75c3328f6ee11c15ab81319e2906c2d1188f
lincesites27 <contato@lincesites.com.br>
2023-01-14 16:43:57 -0300

    fazendo o contador

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 // LINK DO SCROLL,BASTA ADICIONAR AS CLASSES 
-ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.container');
+
+
+
+darkMode = () =>{
+
+    const body = document.querySelector("body");
+    body.classList.toggle("dark")
+}
+
+// Contador automático
+let contar = document.querySelector('#contador');
+		let contador = 0;
+        
+        const contagem = ()=>{
+           
+            setInterval(() => contar.innerHTML = contador++, 1);
+
+                       
+            
+        }
+       
+          clearInterval
+        contagem();
+        
+
+       
